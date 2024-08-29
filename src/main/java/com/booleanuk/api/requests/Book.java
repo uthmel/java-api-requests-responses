8ce2eb0d6ea8f0295abcf86e54f19e590432df64
uthmel <melvin.uthayaseelan@no.experis.com>
2024-08-29 14:11:19 +0200

    adding Book class

---
+++ src/main/java/com/booleanuk/api/requests/Book.java
@@ -0,0 +1,47 @@
+package com.booleanuk.api.requests;
+
+public class Book {
+    private String title;
+    private int numPages;
+    private String author;
+    private String genre;
+
+    public Book(String title, int numPages, String author, String genre) {
+        this.title = title;
+        this.numPages = numPages;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(int numPages) {
+        this.numPages = numPages;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
